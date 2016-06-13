a4296da66862d459391aabe26ed11fed44895818
quangvinh93hcm <vinh.dq@felix.vn>
2016-06-13 23:31:58 +0700

    build infrastructure

---
+++ VinhShop/VinhShop.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VinhShop.Data.Infrastructure
+{
+    public class DbFactory : Disposable, IDbFactory
+    {
+        private VinhShopDbContext dbContext;
+        public VinhShopDbContext Init()
+        {
+            return dbContext ?? (dbContext = new VinhShopDbContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+            {
+                dbContext.Dispose();
+            }
+        }
+    }
+}
