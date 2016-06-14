98661285e5b4f13fddeccbc2066827ca9f48e265
quangvinh93hcm <vinh.dq@felix.vn>
2016-06-14 10:19:01 +0700

    repository class

---
+++ VinhShop/VinhShop.Data/Repositories/PostRepository.cs
@@ -0,0 +1,16 @@
+﻿using VinhShop.Data.Infrastructure;
+using VinhShop.Model.Models;
+
+namespace VinhShop.Data.Repositories
+{
+    public interface IPostRepository : IRepository<Post>
+    {
+    }
+
+    public class PostRepository : RepositoryBase<Post>, IPostRepository
+    {
+        public PostRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
